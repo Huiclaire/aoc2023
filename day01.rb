6e367dab200391f2987a130af7833bc6d1c4a8a6
Claire Lin <chunhuilin7516@gmail.com>
2023-12-02 23:24:38 +0100

    Add word-to-number conversion in to_number method

--- day01.rb
+++ day01.rb
@@ -1,3 +1,47 @@
+# 1. look through array ["twoLnine", "eightwothree"]
+# 2. scan the word matching to number, and take the first and last word => "eight", "three"
+# 3. replace "eight", "three" with "8", "3"
+
+TO_NUM = {
+  'one' => '1',
+  'two' => '2',
+  'three' => '3',
+  'four' => '4',
+  'five' => '5',
+  'six' => '6',
+  'seven' => '7',
+  'eight' => '8',
+  'nine' => '9'
+}
+
+REGEX = /(?=(\d|one|two|three|four|five|six|seven|eight|nine))/
+
+# def letters_to_number(line)
+#   line.map do |word|
+#     TO_NUM.each do |k, v|
+#       matches = word.scan(REGEX)
+#       word.gsub!(k, v)
+#     end
+#     word
+#   end
+# end
+
+def to_number(lines)
+  lines.map do |line|
+    line.scan(REGEX).each do |group|
+      # find first and last number with letter in an array, conver it into digit.
+    end
+  end
+end
+
+lines = ["two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2"]
+p to_number(lines)
+# [[["two"], ["1"], ["nine"]],
+# [["eight"], ["two"], ["three"]],
+# [["one"], ["2"], ["three"]]
+# [["two"], ["one"], ["3"], ["four"]],
+# [["4"], ["nine"], ["eight"], ["seven"], ["2"]]]
+
 def find_first_part(line)
   line.split('').each do |c|
     return c.to_i if /\A\d\z/.match(c)
