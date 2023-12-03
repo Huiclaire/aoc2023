860359debaae323fde0a9a9184bf07b69391c498
Claire Lin <chunhuilin7516@gmail.com>
2023-12-03 13:20:36 +0100

    Add day01 part2

--- day01.rb
+++ day01.rb
@@ -1,7 +1,3 @@
-# 1. look through array ["twoLnine", "eightwothree"]
-# 2. scan the word matching to number, and take the first and last word => "eight", "three"
-# 3. replace "eight", "three" with "8", "3"
-
 TO_NUM = {
   'one' => '1',
   'two' => '2',
@@ -16,31 +12,16 @@ TO_NUM = {
 
 REGEX = /(?=(\d|one|two|three|four|five|six|seven|eight|nine))/
 
-# def letters_to_number(line)
-#   line.map do |word|
-#     TO_NUM.each do |k, v|
-#       matches = word.scan(REGEX)
-#       word.gsub!(k, v)
-#     end
-#     word
-#   end
-# end
-
 def to_number(lines)
   lines.map do |line|
-    line.scan(REGEX).each do |group|
-      # find first and last number with letter in an array, conver it into digit.
+    matches = line.scan(REGEX).flatten.each do |element|
+      TO_NUM.each do |k, v|
+        element.gsub!(k, v)
+      end
     end
+    matches.join
   end
 end
-
-lines = ["two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2"]
-p to_number(lines)
-# [[["two"], ["1"], ["nine"]],
-# [["eight"], ["two"], ["three"]],
-# [["one"], ["2"], ["three"]]
-# [["two"], ["one"], ["3"], ["four"]],
-# [["4"], ["nine"], ["eight"], ["seven"], ["2"]]]
 
 def find_first_part(line)
   line.split('').each do |c|
@@ -57,7 +38,8 @@ def line_value(line)
 end
 
 def total_value(lines)
-  lines.map { |line| line_value(line) }.inject(:+)
+  converted_lines = to_number(lines)
+  converted_lines.map { |line| line_value(line) }.inject(:+)
 end
 
 input = File.readlines('inputs/day01_part1.txt')
