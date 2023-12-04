275179b3446e5eaec4450980608b773f703a9031
Claire Lin <chunhuilin7516@gmail.com>
2023-12-04 20:19:35 +0100

    add day02 part1

--- day02.rb
+++ day02.rb
@@ -1,12 +1,5 @@
-# read each line of the file => ["string", "string"]
-# give ID to all 5 game => each_with_index
-# in ID 1, count every set's number of blue, red, green
-# set up the bag has only 12r, 13gr, 14 blue cubes
-# find out which ID has red <= 12, green <= 13, blue <= 14
-# add all the ID together
-
-COLOR_REGEX = {blue: /(\d+) blue/, red: /(\d+) red/, green: /(\d+) green/}
-COLOR_CONSTRAINTS = {blue: 14, red: 12, green: 13}
+COLOR_REGEX = { blue: /(\d+) blue/, red: /(\d+) red/, green: /(\d+) green/ }
+COLOR_CONSTRAINTS = { blue: 14, red: 12, green: 13 }
 
 result_number = 0
 
@@ -18,25 +11,18 @@ File.readlines('inputs/day02b.txt').each_with_index do |line, idx|
     # color_count[k] = line.scan(v).map(&:to_i).inject(:+)
     color_count[k] = line.scan(v).flatten.map(&:to_i)
   end
-  # p color_count # [4, 5, 12]
 
   color_valid = {}
-  COLOR_CONSTRAINTS.each do |k, v|
-    color_count.each do |color, num|
-      num.each do |n|
-        if n <= v
-          # p "line:#{color} num:#{n}"
-          # p "c:#{k} constraints:#{v}"
-          p color_count[k]
-          p color
-          color_valid[k] = color_count[k]
-        end
-      end
-      # color_valid[k] = (color_count[num] <= v)
+  COLOR_CONSTRAINTS.each do |color, constraint|
+    color_valid[color] = color_count[color].map do |n|
+      n <= constraint
     end
   end
 
-  game_valid = color_valid.values.any?
+  result = color_valid.values.map(&:all?)
+  game_valid = result.all?
   result_number += game_id if game_valid
+
+  p "#{game_id}: #{game_valid}"
+  p result_number
 end
-p result_number
